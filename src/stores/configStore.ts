c512eac0e61c923610281206cba486929d01b083
Muhaddil <markellur08@gmail.com>
2025-08-29 16:13:19 +0200

    style(configStore): improve code formatting and consistency

--- src/stores/configStore.ts
+++ src/stores/configStore.ts
@@ -1,19 +1,19 @@
-import { reactive, computed } from 'vue'
-import { useI18n } from "vue-i18n"
+import { reactive, computed } from "vue";
+import { useI18n } from "vue-i18n";
 
 const { t } = useI18n();
-  
+
 export const configStore = reactive({
-    handleText: computed(() => t("controls.handleText")),
-    titleText: computed(() => t("controls.titleText")),
-    rightHeaderText: computed(() => t("controls.rightHeaderText")),
-    footerText: computed(() => t("controls.footerText")),
-    imageUrl: "",
-    fitImage: "cover",
-  });
+  handleText: computed(() => t("controls.handleText")),
+  titleText: computed(() => t("controls.titleText")),
+  rightHeaderText: computed(() => t("controls.rightHeaderText")),
+  footerText: computed(() => t("controls.footerText")),
+  imageUrl: "",
+  fitImage: "cover",
+});
 
 export function useConfigStore() {
   return {
-    configStore
-  }
+    configStore,
+  };
 }
